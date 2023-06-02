0019db09689e4f70a61a90b30cd4a9d96db65515
imsts <multyanenko@gmail.com>
2023-06-02 10:45:34 +0300

    upd new pages lessons 10 11 ip progress

--- src/route/index.js
+++ src/route/index.js
@@ -18,5 +18,59 @@ router.get('/', function (req, res) {
 
 // ================================================================
 
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/cssLesson10', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('cssLesson10', {
+    layout: 'cssLesson10',
+    page: {
+      title: 'CSS | Lesson10',
+    },
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/cssLesson11', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('cssLesson11', {
+    layout: 'cssLesson11',
+    page: {
+      title: 'CSS | Lesson11',
+    },
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/cssLesson12', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('cssLesson12', {
+    layout: 'cssLesson12',
+    page: {
+      title: 'CSS | Lesson12',
+    },
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
 // Підключаємо роутер до бек-енду
 module.exports = router
