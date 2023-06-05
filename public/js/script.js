a3ca90bb7d22de36ce5484287b6011dd28f710af
imsts <multyanenko@gmail.com>
2023-06-05 09:31:46 +0300

    upd all

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,3 @@
-var codeVisible = false
-var codeElement = document.getElementById('hiddenCode')
-
-function toggleCodeVisibility() {
-  if (codeVisible) {
-    codeElement.style.display = 'none'
-    document.querySelector('button').textContent =
-      'Показати код'
-  } else {
-    codeElement.style.display = 'block'
-    document.querySelector('button').textContent =
-      'Приховати код'
-  }
-
-  codeVisible = !codeVisible
-}
-
 const btn = document.querySelector('.toggleBtn')
 const moveBtn = document.querySelector('.bgMove')
 const fa = document.querySelector('.fa-solid')
@@ -38,14 +21,3 @@ btn.addEventListener('click', function () {
     document.body.classList.remove('dark')
   }
 })
-
-Handlebars.registerHelper('showCode', function (index) {
-  // Повертає true або false в залежності від умови для показу компонента
-})
-
-Handlebars.registerHelper(
-  'getComponentTemplateName',
-  function (index) {
-    // Повертає ім'я шаблону компонента на основі індексу
-  },
-)
--- src/route/index.js
+++ src/route/index.js
@@ -40,42 +40,124 @@ router.get('/cssLesson10', function (req, res) {
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
 router.get('/cssLesson11', function (req, res) {
-  // res.render генерує нам HTML сторінку
+  const data = {
+    cards: [
+      {
+        name: 'Відображення',
+        text: 'display — це CSS властивість, яка визначає відображення елемента на веб-сторінці',
+        code: `
+          .div {
+            display: none;
+            display: block;
+            display: inline;
+            display: inline-block;
+          }
+        `,
+      },
 
-  //            ↙ cюди вводимо назву файлу з сontainer
+      {
+        name: 'Зовнішній відступ',
+        text: 'margin — це CSS властивість, яка задає зовнішні відступи елементу, відносно сусідніх елементів',
+        code: `
+        .div {
+        margin: 10px;
+        }
+        `,
+      },
+
+      {
+        name: 'Внутрішній відступ',
+        text: 'padding — це CSS властивість, яка задає відступи від країв батьківського елементу (внутрішні відступи)',
+        code: `
+        .div {
+        padding: 20px;
+        }
+        `,
+      },
+
+      {
+        name: 'Ширина',
+        text: 'width - це властивість CSS, яка визначає ширину елемента. Ця властивість не застосовується до лінійних елементів',
+        code: `
+        .example {
+        width: 250px;
+        }
+        `,
+      },
+
+      {
+        name: 'Висота',
+        text: 'height - це CSS властивість, яка визначає висоту елемента.',
+        code: `
+        .example {
+        height: max-content;
+        }
+        `,
+      },
+
+      {
+        name: 'Мінімальна ширина',
+        text: 'min-width — це властивість CSS, яка встановлює мінімальну ширину елемента.',
+        code: `
+        .example {
+        min-width: 150px;
+        }
+        `,
+      },
+
+      {
+        name: 'Максимальна ширина',
+        text: 'max-width — це властивість CSS, яка встановлює максимальну ширину елемента.',
+        code: `
+        .example {
+        max-width: 300px;
+        }
+        `,
+      },
+
+      {
+        name: 'Мінімальна висота',
+        text: 'min-height — це властивість CSS, яка встановлює мінімальну висоту елемента.',
+        code: `
+        .example {
+        min-height: 150px;
+        }
+        `,
+      },
+
+      {
+        name: 'Максимальна висота',
+        text: 'max-height — це властивість CSS, яка встановлює максимальну висоту елемента.',
+        code: `
+        .example {
+          max-height: 300px;
+        }
+        `,
+      },
+
+      {
+        name: 'Розмір коробки',
+        text: 'box-sizing — це CSS властивість, яка визначає як розраховувати розмір елемента',
+        code: `
+        .example {
+          box-sizing: content-box;
+        }
+        `,
+      },
+      // Other card objects
+    ],
+  }
+
+  // Render the 'cssLesson11' template and pass the 'data' object
   res.render('cssLesson11', {
     layout: 'cssLesson11',
     page: {
       title: 'CSS | Lesson11',
     },
-
-    main: {
-      content: [
-        {
-          name: 'Відображення',
-          text: 'display — це CSS властивість, яка визначає відображення елемента на веб-сторінці',
-        },
-        {
-          name: 'Зовнішній відступ',
-          text: 'margin — це CSS властивість, яка задає зовнішні відступи елементу, відносно сусідніх елементів',
-        },
-        {
-          name: 'Внутрішній відступ',
-          text: 'padding — це CSS властивість, яка задає відступи від країв батьківського елементу (внутрішні відступи)',
-        },
-        {
-          name: 'Назва',
-          text: 'Пояснення',
-        },
-        {
-          name: 'Назва',
-          text: 'Пояснення',
-        },
-      ],
-    },
+    data: data, // Pass the 'data' object to the template
   })
-  //                  ↑↑ сюди вводимо JSON дані
 })
+//                  ↑↑ сюди вводимо JSON дані
 
 // ================================================================
 
