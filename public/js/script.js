d12aebd9599f9383590ea4218aabd2bc7163f891
imsts <multyanenko@gmail.com>
2023-06-13 13:18:18 +0300

    upd 13/06/23

--- public/js/script.js
+++ public/js/script.js
@@ -11,26 +11,13 @@ for (var i = 0; i < navLinks.length; i++) {
   }
 }
 
-const btn = document.querySelector('.toggleBtn')
-const moveBtn = document.querySelector('.bgMove')
-const fa = document.querySelector('.fa-solid')
-const faMoon = document.querySelector('.fa-moon')
+var toggleBtn = document.getElementById('toggleBtn')
+var body = document.body
 
-btn.addEventListener('click', function () {
-  moveBtn.classList.toggle('active')
-
-  fa.classList.toggle('dark')
-
-  faMoon.classList.toggle('active')
-  if (moveBtn.classList == 'active') {
-    faMoon.classList.add('moon-active')
+toggleBtn.addEventListener('change', function () {
+  if (toggleBtn.checked) {
+    body.classList.add('dark')
   } else {
-    faMoon.classList.remove('moon-active')
-  }
-
-  if (moveBtn.classList.contains('active')) {
-    document.body.classList.add('dark')
-  } else {
-    document.body.classList.remove('dark')
+    body.classList.remove('dark')
   }
 })
